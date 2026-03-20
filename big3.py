2038bb6b41f85f42bcd05e7d0fa4c48e7fe3a54e
arturcatao <arturcataodoo@gmail.com>
2026-03-20 19:19:17 -0300

    update : novo csv so com os rankings do big3

---
+++ big3.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+csv_players = "data/raw/atp_players.csv"
+csv_rankings = "data/clean/atp_rankings_00-20s.csv"
+
+df_players = pd.read_csv(csv_players)
+df_rankings = pd.read_csv(csv_rankings)
+
+players = [
+    ("Roger", "Federer"),
+    ("Rafael", "Nadal"),
+    ("Novak", "Djokovic")
+]
+
+df_big3 = df_players[
+    df_players[["name_first", "name_last"]].apply(tuple, axis=1).isin(players)
+]
+
+ids = df_big3["player_id"].tolist()
+
+df_final = df_rankings[df_rankings["player_id"].isin(ids)]
+
+df_final.to_csv("data/clean/big3_rankings.csv", index=False)
